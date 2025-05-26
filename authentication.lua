f43f5a017b060535b0337ae98d77fad39aa8ea78
Abas-Almas <marzouguiadam@fsm.u-monastir.tn>
2025-05-26 13:33:13 +0100

    main

--- authentication.lua
+++ authentication.lua
@@ -1,12 +1,25 @@
 --create an account
 addCommandHandler("register", function(player, command, username, password)
     if not username or not password then
+<<<<<<< HEAD
         outputChatBox("/register [username]  [password]",player, 235, 64, 52)
     end
 end)
 
 
 --login to their account
+=======
+        return outputChatBox("Usage: /register [username] [password]", player, 235, 64, 52)
+    end
+    if getAccount(username) then
+        return outputChatBox("An account already exixts with that name", player, 255, 100, 100)
+    end
+end)
+--login to their account
+if not isPasswordValid(password) then
+    return outputChatBox("An account already exixts with that name", player, 255, 100, 100)
+end
+>>>>>>> b592c25 (main)
 
 
 --logout of their account
